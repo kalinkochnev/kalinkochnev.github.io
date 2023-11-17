be4845c52b16cc44a9ffc364158e46e98e02889a
kalinkochnev <kalin.kochnev@gmail.com>
2023-11-17 09:18:00 -0500

    updated batch load and project ordering

--- js/batch-load.js
+++ js/batch-load.js
@@ -3,8 +3,14 @@
 // Taken from https://stackoverflow.com/questions/11071314/
 // Triggers when all the batch-loaded images have finished loading
 Promise.all(Array.from(document.querySelectorAll('.batch-load'))
-.filter(img => !img.complete).map(img => new Promise(resolve => {
-    img.onload = img.onerror = resolve;
+.filter(elem => {
+    // Use the browser level api for lazy loading
+    // Don't try to load all images at the same time. Only the ones
+    // marked eager. This is also to prevent conflicting
+    // actions while slideshows try to lazy load
+    return !elem.complete && elem.getAttribute("loading") == "eager"
+}).map(elem => new Promise(resolve => {
+    elem.onload = img.onerror = resolve;
 }))).then(() => {
     setImageOpacity(1)   // Makes the images visible
 });
@@ -16,9 +22,18 @@ setTimeout(function() {
 }, 1750);
 
 function setImageOpacity(opacity) {
-    document.querySelectorAll('.batch-load').forEach(function(image) {
-        image.style.opacity = opacity;
-        image.parentNode.style.backgroundColor = "transparent";
-        image.parentNode.style.animation = "fadeOut 1s";
+    document.querySelectorAll('.batch-load').forEach(function(elem) {
+        // Any content inside a .image-container should only display as
+        // soon as the image loads
+        Array.from(elem.parentNode.childNodes).filter(elem => {
+            /// filter out text nodes
+            return elem.nodeType === 1
+        }).forEach(elem => {
+            console.log(elem)
+            elem.style.opacity=opacity;
+        })
+
+        elem.parentNode.style.backgroundColor = "transparent";
+        elem.parentNode.style.animation = "fadeOut 1s";
     });
 }
